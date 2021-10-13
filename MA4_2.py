164d98750bbb7ed9c8944bf68584d953373bb399
Victoria Gardemalm Niemi <viga7482@siegbahn.it.uu.se>
2021-10-13 09:16:55 +0200

    fixed the code

--- MA4_2.py
+++ MA4_2.py
@@ -3,11 +3,11 @@
 from integer import Integer
 from time import perf_counter as pc
 
-#def fib_py(n):
-	#if n <= 1:
-		#return n
-	#else:
-		#return (fib_py(n-1) + fib_py(n-2))
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return (fib_py(n-1) + fib_py(n-2))
 
 def main():
 	f = Integer(5)
@@ -15,19 +15,19 @@ def main():
 	f.set(7)
 	print(f.get())
 
-	#start1 = pc()
-	#f.fib_py()
-	#print(f.fib_py())
-	#end1 = pc()
+	start1 = pc()
+	resultpy = fib_py(7)
+	print(resultpy)
+	end1 = pc()
 
-	#print(f"fib python took {round(end1-start1, 2)} seconds")
+	print(f"fib python took {round(end1-start1, 5)} seconds")
 
 	start2 = pc()
-	f.fib_cpp()
-	print(f.fib_cpp())
+	resultcpp = f.fib_cpp()
+	print(resultcpp)
 	end2 = pc()
 
-	print(f"fib c++ took {round(end2-start2, 2)} seconds")
+	print(f"fib c++ took {round(end2-start2, 5)} seconds")
 
 if __name__ == '__main__':
 	main()

54d8d79c75f3eeac43798f982c83f90883dc95a6
Victoria Gardemalm Niemi <viga7482@arrhenius.it.uu.se>
2021-10-14 09:45:18 +0200

    added plot of fib 30-45

--- MA4_2.py
+++ MA4_2.py
@@ -2,6 +2,8 @@
 
 from integer import Integer
 from time import perf_counter as pc
+import matplotlib.pyplot as plt
+import numpy as np
 
 def fib_py(n):
 	if n <= 1:
@@ -12,10 +14,30 @@ def fib_py(n):
 def main():
 	f = Integer(5)
 	print(f.get())
-	f.set(47)
+	f.set(7)
 	print(f.get())
 
-	start1 = pc()
+	array = np.arange(30,45)
+	time_py = []
+	time_cpp = []
+
+	for i in array:
+		start1 = pc()
+		fib_py(i)
+		end1 = pc()
+		time_py += [end1-start1]
+
+	for j in array:
+		start2 = pc()
+		f.fib_cpp(j)
+		end2 = pc()
+		time_cpp += [end2-start2]
+
+	plt.plot(array, time_py, 'r-', array, time_cpp, 'b-')
+	plt.show()
+
+
+	"""start1 = pc()
 	resultpy = fib_py(47)
 	print(resultpy)
 	end1 = pc()
@@ -28,6 +50,7 @@ def main():
 	end2 = pc()
 
 	print(f"fib c++ took {round(end2-start2, 5)} seconds")#Took approx 1 minute
+"""
 
 if __name__ == '__main__':
 	main()
